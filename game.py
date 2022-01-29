adaac541497fdfed2498b0033574d027daa37f0b
NickIsola <77946751+NickIsola@users.noreply.github.com>
2022-01-29 16:24:21 -0500

    Determination of winner
    -revised User Input and Computer Choice for data validation reasons.
    -completed rock-paper-scissors calc logics
    -display final output to user

--- game.py
+++ game.py
@@ -4,21 +4,35 @@ import random
 #Intro
 
 print("-------------------")
-print("Hi. Welcome to my Rock-Paper_Scissors game...")
+print("Hi 'Player One' Welcome to my Rock-Paper_Scissors game...")
 print("-------------------")
 # ASK FOR USER INPUT
-
-user_choice = input("Please choose either: 'rock', 'paper', 'scissors': ")
+print("Here are your play options \n 1 (rock) \n 2 (paper) \n 3 (scissors)")
+user_choice = input ("Please select a number corresponding to your desired play: ")
+# user_choice = input("Please choose either: 'rock', 'paper', 'scissors': ")
 
 # VALIDATE USER INPUT
 
-if (user_choice == "rock" or user_choice == "ROCK" or user_choice =="Rock" 
-or user_choice == "paper" or user_choice == "PAPER" or user_choice =="Paper"
-or user_choice == "scissors" or user_choice == "SCISSORS" or user_choice =="Scissors"): 
-    print("You chose:",user_choice)
-else:
-    print("Whoops! That is not a valid input. Please restart the game and try again.")
+if (user_choice != "1" and user_choice != "2" and user_choice != "3"):
+    print("Whoops! That is not a valid play. Please try again.")
     exit()
+if user_choice == "1": 
+    user_choice = "rock"
+    print("You chose: rock")
+elif user_choice == "2": 
+    user_choice = "paper"
+    print("You chose: paper")
+else:
+    user_choice = "scissors"
+    print("You chose: scissors")
+
+# if (user_choice == "rock" or user_choice == "ROCK" or user_choice =="Rock" 
+# or user_choice == "paper" or user_choice == "PAPER" or user_choice =="Paper"
+# or user_choice == "scissors" or user_choice == "SCISSORS" or user_choice =="Scissors"): 
+#     print("You chose:",user_choice)
+# else:
+#     print("Whoops! That is not a valid play. Please check your spelling and try again.")
+#     exit()
 
 # COMPUTER CHOICE
 
@@ -29,5 +43,25 @@ print("-------------------")
 
 # DETERMINE THE WINNER
 
-# FINAL RESULTS
+if user_choice == computer_choice: 
+    print("It's a tie!")
+elif user_choice == "rock": 
+    if computer_choice == "scissors":
+        print("You win! Congrats")
+    else: 
+        print("The computer won. It's ok")
+elif user_choice == "scissors":
+    if computer_choice == "paper":
+        print("You win! Congrats")
+    else: 
+        print("The computer won. It's ok")
+elif user_choice == "paper":
+    if computer_choice == "rock": 
+        print("You win! Congrats")
+    else: 
+        print("The computer won. It's ok")
+
+# FAREWELL
+print("-------------------")
+print("Thanks for playing. Please play again soon!")
 
