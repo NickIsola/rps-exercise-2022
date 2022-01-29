f4baf2b7caba761b255e85a0736e1cc61420a612
NickIsola <77946751+NickIsola@users.noreply.github.com>
2022-01-29 15:29:44 -0500

    Computer Choice
    Also added:
    -also added dashed line separators for aesthetics
    -imported random package

--- game.py
+++ game.py
@@ -1,31 +1,31 @@
+#Import necessary packages
+import random  
 
+#Intro
 
-
-#
-# todo: write some Python code here to satisfy the exercise objectives
-# ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
-#
-
-
-
-
+print("-------------------")
+print("Hi. Welcome to my Rock-Paper_Scissors game...")
+print("-------------------")
 # ASK FOR USER INPUT
 
-user_choice = input("Please choose one of: 'rock', 'paper', 'scissors': ")
+user_choice = input("Please choose either: 'rock', 'paper', 'scissors': ")
 
 # VALIDATE USER INPUT
 
 if (user_choice == "rock" or user_choice == "ROCK" or user_choice =="Rock" 
 or user_choice == "paper" or user_choice == "PAPER" or user_choice =="Paper"
 or user_choice == "scissors" or user_choice == "SCISSORS" or user_choice =="Scissors"): 
-    print("User Choice:",user_choice)
+    print("You chose:",user_choice)
 else:
     print("Whoops! That is not a valid input. Please restart the game and try again.")
     exit()
 
 # COMPUTER CHOICE
 
-
+computer_options = ["rock",'paper','scissors']
+computer_choice = random.choice(computer_options)
+print("The computer chose:", computer_choice)
+print("-------------------")
 
 # DETERMINE THE WINNER
 
