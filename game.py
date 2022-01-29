b259a8704c0e0f21667adebedc132111459ebf42
NickIsola <77946751+NickIsola@users.noreply.github.com>
2022-01-28 20:23:42 -0500

    Asked for user input

--- game.py
+++ game.py
@@ -5,3 +5,21 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+
+
+
+
+# ASK FOR USER INPUT
+
+userChoice = input("Please choose one of: 'rock', 'paper', 'scissors': ")
+print("User Choice:",userChoice)
+
+
+# VALIDATE USER INPUT
+
+# COMPUTER CHOICE
+
+# DETERMINE THE WINNER
+
+# FINAL RESULTS
+
