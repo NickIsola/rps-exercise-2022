3b0af8af775eccc0c586283d51895b8c2de9c0ce
NickIsola <77946751+NickIsola@users.noreply.github.com>
2022-02-03 21:47:28 -0500

    Final Touches to Base Requirements
    -miscellaneous minor edits

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
-#IMPORT PACKAGES
+#IMPORT RELEVANT PACKAGES
+
 import random  
 import os
 
 # ALLOWS FOR A USER NAME
+
 PLAYER_NAME = os.getenv("PLAYER_NAME", default="Player One")
 
 #INTRO
@@ -12,7 +14,9 @@ print("Hi",PLAYER_NAME +"!", "Welcome to my Rock-Paper-Scissors game...")
 print("-------------------")
 
 # ASK FOR USER INPUT
-print("Here are your play options \n 1 (rock) \n 2 (paper) \n 3 (scissors)")
+    #OFFER NUMBER OPTIONS TO HELP WITH DATA VALIDATION 
+
+print("Here are your play options: \n 1 (rock) \n 2 (paper) \n 3 (scissors)")
 user_choice = input ("Please select a number corresponding to your desired play: ")
     # user_choice = input("Please choose either: 'rock', 'paper', 'scissors': ")
 
@@ -23,13 +27,13 @@ if (user_choice != "1" and user_choice != "2" and user_choice != "3"):
     exit()
 if user_choice == "1": 
     user_choice = "rock"
-    print("You chose: rock")
+    print("You chose:", user_choice)
 elif user_choice == "2": 
     user_choice = "paper"
-    print("You chose: paper")
+    print("You chose:", user_choice)
 else:
     user_choice = "scissors"
-    print("You chose: scissors")
+    print("You chose:", user_choice)
 
     # if (user_choice == "rock" or user_choice == "ROCK" or user_choice =="Rock" 
     # or user_choice == "paper" or user_choice == "PAPER" or user_choice =="Paper"
@@ -68,4 +72,4 @@ elif user_choice == "paper":
 
 # FAREWELL
 print("-------------------")
-print("Thanks for playing. Please play again soon!")
+print("Thanks for playing", PLAYER_NAME+".", "Please play again soon!")
