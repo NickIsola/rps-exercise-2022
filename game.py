f7de3a6e987b458a88796f1ce87d8362bfb40202
NickIsola <77946751+NickIsola@users.noreply.github.com>
2022-02-04 15:54:56 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,36 +1,29 @@
 # "game.py" FILE
 
-# "determine_winner" function defintion
+# "determine_winner" FUNCTION DEFINTION
 def determine_winner(user_choice,computer_choice):
-    # determine the winner logic
+    # DETERMINE THE WINNER LOGIC
     outcome = ""
     if user_choice == computer_choice: 
         outcome = "It's a tie!"
-        # print("It's a tie!")
     elif user_choice == "rock": 
         if computer_choice == "scissors":
             outcome = "You win! Congrats"
-            # print("You win! Congrats")
         else: 
             outcome = "The computer won. It's ok"
-            # print("The computer won. It's ok")
     elif user_choice == "scissors":
         if computer_choice == "paper":
             outcome = "You win! Congrats"
-            # print("You win! Congrats")
         else: 
             outcome = "The computer won. It's ok"
-            # print("The computer won. It's ok")
     elif user_choice == "paper":
         if computer_choice == "rock": 
             outcome = "You win! Congrats"
-            # print("You win! Congrats")
         else: 
             outcome = "The computer won. It's ok"
-            # print("The computer won. It's ok")
     return outcome
 
-#IMPORT RELEVANT PACKAGES
+# IMPORT RELEVANT PACKAGES
 import random  
 import os
 
@@ -41,18 +34,17 @@ PLAYER_NAME = os.getenv("PLAYER_NAME", default="Player One")
 # MAIN CONDITIONAL
 if __name__ == "__main__":
 
-    #INTRO
+    # INTRO
     print("-------------------")
     print("Hi",PLAYER_NAME +"!", "Welcome to my Rock-Paper-Scissors game...")
     print("-------------------")
 
     # ASK FOR USER INPUT
-        #OFFER NUMBER OPTIONS TO HELP WITH DATA VALIDATION 
+        # NUMBER OPTIONS TO HELP WITH DATA VALIDATION
     print("Here are your play options: \n 1 (rock) \n 2 (paper) \n 3 (scissors)")
     user_choice = input ("Please select a number corresponding to your desired play: ")
-        # user_choice = input("Please choose either: 'rock', 'paper', 'scissors': ")
 
-    # VALIDATE USER INPUT 
+    # VALIDATE USER INPUT
     if (user_choice != "1" and user_choice != "2" and user_choice != "3"):
         print("Whoops! That is not a valid play. Please try again.")
         exit()
@@ -66,21 +58,14 @@ if __name__ == "__main__":
         user_choice = "scissors"
         print("You chose:", user_choice)
 
-        # if (user_choice == "rock" or user_choice == "ROCK" or user_choice =="Rock" 
-        # or user_choice == "paper" or user_choice == "PAPER" or user_choice =="Paper"
-        # or user_choice == "scissors" or user_choice == "SCISSORS" or user_choice =="Scissors"): 
-        #     print("You chose:",user_choice)
-        # else:
-        #     print("Whoops! That is not a valid play. Please check your spelling and try again.")
-        #     exit()
-
     # COMPUTER CHOICE
     computer_options = ["rock",'paper','scissors']
     computer_choice = random.choice(computer_options)
     print("The computer chose:", computer_choice)
     print("-------------------")
 
-    #...
+    # CALL THE "determine_winner" FUNCTION, PASSING IN USER AND COMPUTER PLAYS
+        # ASSIGN RETURN VALUE TO VARIABLE
     outcome = determine_winner(user_choice, computer_choice)
 
     # FAREWELL
